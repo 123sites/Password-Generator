8e8d022ed5d2f877c01b14ada3ed5f54cfd673bf
CF <cfreitas12345@gmail.com>
2023-01-11 12:39:04 -0700

    Added function get to all the criteria.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Assignment Code
+
 // 8-128 characters long
-// Confirm whether or not to include lowrcase, upper, numneric, and/or special characters.
+// Confirm whether or not to include lowrcase, upper, numeric, and/or special characters.
 // Input should be validated & at least 1 character type should be selected.
 // Afterwards, a password is displayed in an alert or written to the page.
 
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
-  console.log("Generate Password button clicked");
+  console.log("Generate Password button clicked.");
 
 return "Password will go here.";
 }
@@ -18,9 +19,26 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
+function getRandomLower () {
+}
+console.log(getRandomLower())
+
+
+function getRandomUpper () {
+}
+console.log(getRandomUpper())
+
+
+function getRandomNumber () {
+}
+console.log(getRandomNumber())
+
+
+function getRandomSpecialCharacters () {
+}
+console.log(getRandomSpecialCharacters())
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
