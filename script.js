7be524042e68c356f667f797659530ec371b3247
CF <cfreitas12345@gmail.com>
2023-01-11 15:24:46 -0700

    The symbol generator code works well.

--- script.js
+++ script.js
@@ -17,18 +17,23 @@ function generatePassword(){
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  // const generatePassword = (length, hasLowercase, hasUppercase, hasNumbers, hasSymbols,)
   passwordText.value = password;
 }
 
 // 1-RandomLower
+let lower = "a"
 function getRandomLower () {
+  const lower = ["asdfghjklqwertyuiopzxcvbnm"];
+  return String.fromCharCode(Math.floor(Math.random()) * 26 + 97);
 }
 console.log(getRandomLower())
 
 
 // 2-RandomUpper
+let upper = "A"
 function getRandomUpper () {
+  return String.fromCharCode(Math.floor(Math.random()) * 26 + 65);
 }
 console.log(getRandomUpper())
 
@@ -39,10 +44,15 @@ console.log(getRandomUpper())
 //   max - Math.floor(9);
 //   return Math.floor(Math.random() * (max - min + 1) + min); 
 // }
-let x = Math.floor((Math.random() * 9) + 0);
+let number = "0"
+function getRandomNumber () {
+  const number = []
+  return String.fromCharCode(Math.floor(Math.random()) * 26 + 48);
 
+}
 
 // 4-RandomSpecialCharacters
+// THIS WORKS!!!!!!!!!!!!!!!!!!!!
 let symbol = "?"
 function getRandomSymbol () {
   //const characters = '~!@#$%^&*()_+=:;<>/?.,';
